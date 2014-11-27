3f845b94f8921d645cc00837d810d643f0c809d4
Moritz Kaufmann <moritz.kaufmann@tum.de>
2014-11-27 08:41:03 +0100

    Use for loop instead of loop + match

--- src/main.rs
+++ src/main.rs
@@ -15,51 +15,42 @@ use test::Bencher;
 #[deriving(Show)]
 enum Exp {
     INum(i32),
-    FNum(f64), 
+    FNum(f64),
     Sym(String),
     QSym(String),
     Sexp(Vec<Exp>),
 }
 
-fn parse_sexp<'a> (re:Regex, reiter:&mut regex::FindCaptures, sexp:&'a str) -> Vec<Exp> {
-    let mut vs: Vec<Exp> = Vec::new();
-    
-    loop {
-        match reiter.next() {
-            Some(cap) => { //captures
+fn parse_sexp<'a> (re:Regex, sexp:&'a str) -> Option<Vec<Exp>> {
+    let mut exp_stack = vec![];
+    exp_stack.push(vec![]);
 
-                let lp =  cap.name("lp"); 
-                let rp =  cap.name("rp"); 
-                let s =  cap.name("s");
-                let qs =  cap.name("qs"); 
-                let inum =  cap.name("inum"); 
-                let fnum =  cap.name("fnum");
+    for cap in re.captures_iter(sexp) {
+        if !cap.name("lp").is_empty() {
+            exp_stack.push(vec![]);
+        } else if !cap.name("rp").is_empty() {
+            let exprs = exp_stack.pop();
+            let scope = exp_stack.last_mut().expect("Requires scope");
+            scope.push(Exp::Sexp(exprs.unwrap()));
+        } else {
+            let s =  cap.name("s");
+            let qs =  cap.name("qs");
+            let inum =  cap.name("inum");
+            let fnum =  cap.name("fnum");
 
-                if lp != "" {
-                    let rvs = parse_sexp(re.clone(), reiter, sexp);
-                    vs.push(Exp::Sexp(rvs));
-                }
-                else if s != "" {vs.push(Exp::Sym(s.to_string()));}
+            let scope = exp_stack.last_mut().expect("Requires scope");
+            if !s.is_empty() {scope.push(Exp::Sym(s.to_string()));}
+            else if !qs.is_empty() {scope.push(Exp::QSym(qs.to_string()));}
+            else if !inum.is_empty() {scope.push(Exp::INum(from_str::<i32>(inum).unwrap()));}
+            else if !fnum.is_empty() {scope.push(Exp::FNum(from_str::<f64>(fnum).unwrap()));}
+            else {panic!("unknown token! {}",cap.at(0));}
+        }
+    }
 
-                
-                else if qs != "" {vs.push(Exp::QSym(qs.to_string()));}
-
-                
-                else if inum != "" {vs.push(Exp::INum(from_str::<i32>(inum).unwrap()));}
-
-                else if fnum != "" {vs.push(Exp::FNum(from_str::<f64>(fnum).unwrap()));}                
-
-                else if rp != "" {break;}
-
-                else {panic!("unknown token! {}",cap.at(0));}
-            },
-            None => break
-        };
-    }                                            
-    vs
+    exp_stack.pop()
 }
 
-fn parse(expr: &str) -> Vec<Exp> {
+fn parse(expr: &str) -> Option<Vec<Exp>> {
     let sxre = r#"(?P<lp>\()|(?P<rp>\))|(?P<qs>".*?"+)|(?P<num>(?P<fnum>-?\d+\.\d+)|(?P<inum>-?\d+))|(?P<s>[[:alnum:]+|[:punct:]+|[:graph:]+]+)"#;
 
     let re = match Regex::new(sxre){
@@ -67,8 +58,7 @@ fn parse(expr: &str) -> Vec<Exp> {
         Err(err) => panic!("{}", err),
     };
 
-    let mut reiter = re.captures_iter(expr);
-    parse_sexp(re.clone(), &mut reiter,expr)
+    parse_sexp(re.clone(), expr)
 }
 
 fn main () {
@@ -113,3 +103,11 @@ fn bench_medium(b: &mut Bencher) {
 // No Box
 // test bench_medium ... bench:    777011 ns/iter (+/- 22575)
 // test bench_small  ... bench:    231762 ns/iter (+/- 6020)
+
+// No Box + No Recursion
+// test bench_medium ... bench:    532756 ns/iter (+/- 25839)
+// test bench_small  ... bench:    224482 ns/iter (+/- 10891)
+
+// No Box + No Recursion + For Loop
+// test bench_medium ... bench:    513237 ns/iter (+/- 28470)
+// test bench_small  ... bench:    217787 ns/iter (+/- 9845)
